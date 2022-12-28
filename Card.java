93143424520498723711d7c5457924dc84ef1145
Ediz Arkın Kobak <119970256+EdizArkin@users.noreply.github.com>
2022-12-28 15:18:16 +0300

    Card class
    the propretys of cards are defined

---
+++ Card.java
@@ -0,0 +1,27 @@
+public class Card {
+    // the propertys of the cards are defined
+    public String suit;
+    public String value;
+    public Card(String suit, String value){
+        this.value = value;
+        this.suit = suit;
+    }
+    public String getSuit() {
+        return suit;
+    }
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+    public String getValue() {
+        return value;
+    }
+    public void setValue(String value) {
+        this.value = value;
+    }
+    @Override
+    public String toString() {
+        String output = suit+value;
+        return output;
+    }
+
+}
